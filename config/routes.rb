a0a92f72263819a62e18076f2dca1f12fe13318b
laura prattico <lauraprattico@hotmail.com>
2024-02-08 09:31:55 +0000

    complete yelp MPV

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,29 @@
 Rails.application.routes.draw do
+  get "up", to: "rails/health#show", as: :rails_health_check
+
+  root to: "restaurants#index"
+
+  resources :restaurants, only: [:new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
+
+end
+
+  # resources :restaurant do
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
+  # get "up" => "rails/health#show", as: :rails_health_check
   # Defines the root path route ("/")
   # root "posts#index"
-end
+
+  # Read all
+  # root to: "restaurants#index"
+  # Create a new restaurant
+  # get "restaurants/new", to: "restaurants#new",  as: :new_restaurant
+  # post "restaurants", to: "restaurants#create"
+  # See details of a restaurant:
+  # get "restaurants/:id", to: "restaurants#show", as: :restaurant
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+dovetail = {name: "The dovetail", address: "E2 7JE London", phone_number: "0761243567", category: "belgian", rating: 5, content: "Great customer service"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "0776579223", category: "italian", rating: 4, content: "Delicious"}
+epicure = {name: "Epicure", address: "75008 Paris", phone_number: "0721349875", category: "french", rating: 3, content:"Amazing dessert"}
+happy_lamb = {name: "Happy Lamb", address: "SE45 3FQ London", phone_number: "0751200987", category: "chinese", rating: 5, content: "Great hotpot restaurant" }
+shoryu = {name: "shoryu", address: "SW1Y 4LR London", phone_number: "0751209834", category: "japanese", rating: 2, content: "Long waiting time"}
+
+
+[dovetail, pizza_east, epicure, happy_lamb, shoryu].each do |attributes|
+  restaurant = Restaurant.create!(name: attributes[:name], address: attributes[:address], phone_number: attributes[:phone_number], category: attributes[:category])
+  restaurant.reviews.create!(rating: attributes[:rating], content: attributes[:content])
+  puts "Created #{restaurant.name} with a rating of #{attributes[:rating].to_i}"
+end
